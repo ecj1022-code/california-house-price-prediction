50c3a91213706db86df7bb6378bc27ff6f05e71b
unknown <ecj1022@gmail.com>
2026-04-10 21:49:37 +0530

    update yml file2

--- train.py
+++ train.py
@@ -1,31 +1,25 @@
-import json
-
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-import  joblib
 import os
+import pandas as pd
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import joblib
 def main():
-    iris = load_iris()
-    X , y= iris.data, iris.target
-    print(X)
-    print(y)
-    print(iris.target_names)
+    data = fetch_california_housing()
+    X = pd.DataFrame(data.data, columns=data.feature_names)
+    y = data.target
+    # print(X.head())
+    # print(y)
     X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
 
-    model = LogisticRegression(max_iter=200)
+    # Train
+    model = LinearRegression()
     model.fit(X_train,y_train)
 
+    # Save the model
     os.mkdir("artifacts")
-    model_path = os.path.join("artifacts","model.pkl")
-    joblib.dump(model,model_path)
-
-    acc = model.score(X_test, y_test)
-    metrics = {"accuracy":float(acc)}
-
-    with open(os.path.join("artifacts","metrics.json"),"w") as f:
-        json.dump(metrics,f)
-    print(acc)
+    model_path = os.path.join("artifacts", "model.pkl")
+    joblib.dump(model, model_path)
 
 
 if __name__ == '__main__':
